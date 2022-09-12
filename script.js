90c8fd333f165521d1c018ae634a4c2d4450d849
Chinazaekpere-Ike <marylynike@gmail.com>
2022-09-13 00:58:08 +0100

    Added the Done,  Edit, Delete buttons to the list preview

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function formActions(event) {
   let task = formInput.value;
 
   if (task.length < 1) {
-    alert("Please fill a valid task");
+    alert("Please write something");
     return;
   }
 
@@ -21,9 +21,32 @@ function formActions(event) {
   task_input_el.value = task;
   task_input_el.setAttribute("readonly", "readonly");
 
+  let task_actions_el = document.createElement("div");
+  task_actions_el.classList.add("actions");
+
+  let task_done_el = document.createElement("button");
+  task_done_el.classList.add("done");
+  task_done_el.innerHTML = `<i class="fa-solid fa-check"></i>`;
+
+  let task_edit_el = document.createElement("button");
+  task_edit_el.classList.add("edit");
+  task_edit_el.innerHTML = `<i class="fa-solid fa-pencil"></i>`;
+
+  let task_delete_el = document.createElement("button");
+  task_delete_el.classList.add("delete");
+  task_delete_el.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
+
+  task_el.appendChild(task_done_el);
+
   taskList.appendChild(task_el);
   task_el.appendChild(task_content_el);
   task_content_el.appendChild(task_input_el);
+
+  task_el.appendChild(task_actions_el);
+  task_actions_el.appendChild(task_edit_el);
+  task_actions_el.appendChild(task_delete_el);
+
+  formInput.value = "";
 }
 
 function gettingHTML() {
