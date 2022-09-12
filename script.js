0485e795b4d5378474c9ad96127f22538b22c1d0
Chinazaekpere-Ike <marylynike@gmail.com>
2022-09-13 00:31:15 +0100

    Created Preview of written lists in JS

--- script.js
+++ script.js
@@ -6,9 +6,24 @@ function formActions(event) {
 
   if (task.length < 1) {
     alert("Please fill a valid task");
-  } else {
-    console.log("success");
+    return;
   }
+
+  let task_el = document.createElement("div");
+  task_el.classList.add("task");
+
+  let task_content_el = document.createElement("div");
+  task_content_el.classList.add("content");
+
+  let task_input_el = document.createElement("input");
+  task_input_el.classList.add("text");
+  task_input_el.type = "text";
+  task_input_el.value = task;
+  task_input_el.setAttribute("readonly", "readonly");
+
+  taskList.appendChild(task_el);
+  task_el.appendChild(task_content_el);
+  task_content_el.appendChild(task_input_el);
 }
 
 function gettingHTML() {
